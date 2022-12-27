8dadbd766414ac79bfaeaef25623da0877aabc05
Jorge Ruiz <jorge.ruiz020899@gmail.com>
2022-12-26 20:12:57 -0500

    * Modify components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import logo1 from "./logo1.png";
 import "./App.css";
-import ListOfChar from "./components/ListOfCharacters";
+import { useEffect, useState } from "react";
+import Characters from "./components/Characters";
+import getChar from "./services/getCharacters";
 
 function App() {
+  const [char, setChar] = useState([]);
+
+  useEffect(() => {
+    getChar().then((char) => setChar(char));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,8 +17,16 @@ function App() {
         <p>
           <strong>Bienvenido a la API de Rick y Morty</strong>
         </p>
-        <section className="Components">
-          <ListOfChar />
+        <section className="Grid">
+          {char.map(({ id, name, image, locationName, status }) => (
+            <Characters
+              key={id}
+              name={name}
+              image={image}
+              locationName={locationName}
+              status={status}
+            />
+          ))}
         </section>
       </header>
     </div>
